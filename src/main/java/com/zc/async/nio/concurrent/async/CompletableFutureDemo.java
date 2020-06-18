05881794b823032fce66fa60e6ce2345fb25e10a
coderzc <zc1217zc@126.com>
2020-06-18 20:38:19 +0800

    async

--- src/main/java/com/zc/async/nio/concurrent/async/CompletableFutureDemo.java
+++ src/main/java/com/zc/async/nio/concurrent/async/CompletableFutureDemo.java
@@ -14,9 +14,73 @@ import org.slf4j.LoggerFactory;
 /**
  * @author coderzc
  * Created on 2020-06-12
+ * 创建无返回异步任务
+ * runAsync
+ *
+ * 创建有返回异步任务
+ * supplyAsync
+ *
+ * 异步任务正常执行完或者抛出异常时
+ * whenComplete
+ * whenCompleteAsync
+ *
+ * 异步任务抛出异常时
+ * exceptionally
+ *
+ * 异步任务串行化,前一个有返回异步任务正常执行完,返回值作为下一个有参有返回异步任务的参数
+ * thenApply
+ * thenApplyAysnc
+ *
+ * 异步任务串行化,前一个有返回异步任务正常执行完或者抛出异常时,返回值作为下一个有参有返回异步任务的参数
+ * handle
+ * handleAsync
+ *
+ * 异步任务串行化,前一个有返回异步任务正常执行完,返回值作为下一个有参无返回异步任务的参数
+ * thenAccept
+ * thenAcceptAsync
+ *
+ * 异步任务串行化,前一个异步任务正常执行完,执行下一个无参无返回的异步任务
+ * thenRun
+ * thenRunAsync
+ *
+ * 整合异步任务,两个异步任务都执行完,把两个异步任务的结果放到一块处理, 有参有返回
+ * thenCombine
+ * thenCombineAsync
+ *
+ * 整合异步任务,两个异步任务都执行完,把两个异步任务的结果放到一块处理, 有参无返回
+ * thenAcceptBoth
+ * thenAcceptBothAsync
+ *
+ * 整合异步任务,哪个返回结果快就使用哪个结果,有参有返回
+ * applyToEither
+ * applyToEitherAsync
+ *
+ * 整合异步任务,哪个返回结果快就使用哪个结果,有参无返回
+ * acceptEither
+ * acceptEitherAsync
+ *
+ * 两个异步任务,任何一个执行完成了都会执行下一步操作,无参无返回
+ * runAfterEither
+ * runAfterEitherAsync
+ *
+ * 两个异步任务,都完成了才会执行下一步操作,无参无返回
+ * runAfterBoth
+ * runAfterBothAsync
+ *
+ * 所有的异步任务都完成
+ * allOf
+ *
+ * 任意一个异步任务完成
+ * anyOf
+ *
+ * 获取异步任务结果
+ * get
+ * join
+ * 区别:get会抛异常, join不会抛异常
+ *
  * 以 Async 结尾的都是提交至 asyncPool 执行，不带的如果准备执行时已经任务已经完成则使用用户线程执行，如果没有完成则使用提交至任务线程等待执行
- * whenComplete、handle 返回原始的CompletableFuture
- * thenApply、thenAccept、thenRun、 exceptionally 返回新的CompletableFuture (使用原始的CompletableFuture.get()/join() 不能阻塞住)
+ * whenComplete(BiConsumer <value,err>)、handle(BiFunction<value,err><value>)、thenRun(Runnable)、exceptionally（但是依然会抛出异常） 返回相同的CompletableFuture
+ * thenApply(Function<value><valueNew>)、thenAccept(Consumer<value><Void>) 返回不同类型的CompletableFuture (使用原始的CompletableFuture.get()/join() 不能阻塞住)
  */
 public class CompletableFutureDemo {
     private long start;
@@ -60,6 +124,23 @@ public class CompletableFutureDemo {
     }
 
     @Test
+    public void testExceptionally(){
+        CompletableFuture<Double> cf = CompletableFuture.supplyAsync(CompletableFutureDemo::fetchPrice, executor);
+        try {
+            cf.exceptionally(e -> {
+                logger.error("err:", e);
+                return 0.0;
+            });
+
+            CompletableFuture.allOf(cf).get();
+        } catch (Exception e) {
+            logger.error("err:", e);
+        } finally {
+            executor.shutdown();
+        }
+    }
+
+    @Test
     public void testWhenComplete() {
         CompletableFuture<Double> cf = CompletableFuture.supplyAsync(CompletableFutureDemo::fetchPrice, executor);
         try {
@@ -69,9 +150,6 @@ public class CompletableFutureDemo {
                     return;
                 }
                 logger.info("result:" + v);
-            }).exceptionally(e -> {
-                logger.error("err:", e);
-                return 0.0;
             });
 
             CompletableFuture.allOf(cf).get();
@@ -133,13 +211,10 @@ public class CompletableFutureDemo {
             }).thenApply(x -> {
                 logger.info("thenApply2:{}", x);
                 return (int) x.doubleValue() + 1;
-            }).exceptionally(err -> {
-                logger.error("err:", err);
-                throw new RuntimeException("exceptionally catch err", err);
-            }).get();
+            });
 
-            System.out.println(cf.get());
-
+            cf.get();
+//            CompletableFuture.allOf(cf).get();
         } catch (Exception e) {
             logger.error("err:", e);
         } finally {
@@ -169,7 +244,7 @@ public class CompletableFutureDemo {
         try {
             cf.thenRun(() -> {
                 logger.info("thenRun:----");
-            }).join();
+            });
 
             CompletableFuture.allOf(cf).get();
         } catch (Exception e) {
@@ -187,7 +262,7 @@ public class CompletableFutureDemo {
             } catch (InterruptedException e) {
                 logger.error("err:", e);
             }
-            if (Math.random() < 0.2) {
+            if (Math.random() < 0.9) {
                 throw new RuntimeException("fetch price failed!");
             }
             return 5 + Math.random() * 20;
